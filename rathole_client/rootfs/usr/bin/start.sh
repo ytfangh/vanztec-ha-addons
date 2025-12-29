42ab494059d385cb6e5e4d2521c33c2a58e18b2b
ytfangh <fanyuntaoca@gmail.com>
2025-12-29 14:24:26 -0500

    Update start.sh

--- rathole_client/rootfs/usr/bin/start.sh
+++ rathole_client/rootfs/usr/bin/start.sh
@@ -21,6 +21,43 @@ EOF
 bashio::log.info "Starting rathole client_id=${CLIENT_ID}"
 bashio::log.info "client.toml: $(cat /tmp/client.toml | tr '\n' ' ')"
 
+# --------------------------------------------------
+# Detect arch
+# --------------------------------------------------
+ARCH_RAW="$(uname -m)"
+case "$ARCH_RAW" in
+  x86_64)
+    ARCH="x86_64"
+    ;;
+  aarch64)
+    ARCH="aarch64"
+    ;;
+  armv7l)
+    ARCH="armv7"
+    ;;
+  armv6l)
+    ARCH="arm"
+    ;;
+  *)
+    ARCH="unknown (${ARCH_RAW})"
+    ;;
+esac
+
+# --------------------------------------------------
+# Detect libc (glibc vs musl)
+# --------------------------------------------------
+if command -v ldd >/dev/null 2>&1 && ldd --version 2>&1 | grep -qi musl; then
+  LIBC="musl"
+else
+  LIBC="glibc"
+fi
+
+bashio::log.info "System arch detected: ${ARCH}"
+bashio::log.info "System libc detected: ${LIBC}"
+
+# --------------------------------------------------
+# Rathole diagnostics
+# --------------------------------------------------
 bashio::log.info "rathole version: $(/usr/local/bin/rathole --version 2>&1 || true)"
 bashio::log.info "rathole help: $(/usr/local/bin/rathole -h 2>&1 | head -n 5 | tr '\n' ' ' || true)"
 
@@ -31,13 +68,17 @@ export RUST_BACKTRACE=1
 bashio::log.info "rathole file info:"
 ls -l /usr/local/bin/rathole || true
 
-bashio::log.info "arch: $(uname -m)"
-bashio::log.info "trying to run rathole..."
+bashio::log.info "uname -m raw: ${ARCH_RAW}"
+
+bashio::log.info "trying to run rathole --version..."
 /usr/local/bin/rathole --version 2>&1 || true
 
 bashio::log.info "ldd check:"
 ldd /usr/local/bin/rathole 2>&1 || true
 
+# --------------------------------------------------
+# Exec rathole
+# --------------------------------------------------
 bashio::log.info "now exec rathole..."
 exec /usr/local/bin/rathole /tmp/client.toml 2>&1
 
